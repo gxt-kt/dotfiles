c7204a7882e7dc62b9fe9d030cfa27569f52f913
gxt_kt <gxt_kt.com>
2023-07-25 03:53:57 +0800

    update i3 and dunst

--- i3/.config/i3/command.sh
+++ i3/.config/i3/command.sh
@@ -8,12 +8,14 @@ then
     # 静音状态
     qdbus org.kde.plasmashell /org/kde/osdService org.kde.osdService.volumeChanged 0
     notify-send "Volume Mute!" -i /usr/share/icons/Mkos-Big-Sur/128x128/devices/audiocard_mute.png -u critical -t 1000
+    bash ~/.scripts/dunst/sound-normal.sh 
     echo "off"
   else
     # 非静音状态
     volume=$(amixer get Master | grep -o -E '[0-9]{1,3}%' | head -1 | cut -d'%' -f1)
     qdbus org.kde.plasmashell /org/kde/osdService org.kde.osdService.volumeChanged ${volume}
     notify-send -a "Volume" -i /usr/share/icons/Mkos-Big-Sur/128x128/devices/audiocard.png -h int:value:${volume} "Volume"
+    bash ~/.scripts/dunst/sound-normal.sh 
     echo ${volume} 
   fi
 elif [ "$1" == "backlight" ]
