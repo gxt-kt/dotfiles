7445f94fd81fd5a7ab2ab0d2e82c9aa070970688
gxt_kt <gxt_kt.com>
2023-09-22 22:23:09 +0800

    add statuscol plugin and settings

--- lvim/.config/lvim/plugins.lua
+++ lvim/.config/lvim/plugins.lua
@@ -851,30 +851,94 @@ lvim.plugins = {
   --     })
   --   end
   -- }
-  -- {
-  -- "luukvbaal/statuscol.nvim",
-  -- config = function()
-  --   local builtin = require("statuscol.builtin")
-  --   require("statuscol").setup({
-  --     -- configuration goes here, for example:
-  --     relculright = true,
-  --     segments = {
-  --       { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-  --       {
-  --         sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
-  --         click = "v:lua.ScSa"
-  --       },
-  --       { text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
-  --       {
-  --         sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
-  --         click = "v:lua.ScSa",
-  --         sign = { name = { "gitsign*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
-  --         click = "v:lua.ScSa",
-  --       },
-  --     }
-  --   })
-  -- end,
-  -- },
+  {
+    "luukvbaal/statuscol.nvim",
+    config = function()
+      local builtin = require("statuscol.builtin")
+      require("statuscol").setup({
+        -- segments = {
+        --   {
+        --     text = {
+        --       " ",     -- whitespace padding
+        --       function(args) -- custom line number highlight function
+        --         return ((args.lnum % 2 > 0) and "%#DiffDelete#%=" or "%#DiffAdd#%=") .. "%l"
+        --       end,
+        --       " ", -- whitespace padding
+        --     },
+        --     condition = {
+        --       true,    -- always shown
+        --       function(args) -- shown only for the current window
+        --         return vim.api.nvim_get_current_win() == args.win
+        --       end,
+        --       builtin.notempty, -- only shown when the rest of the statuscolumn is not empty
+        --     },
+        --   }
+        -- },
+        setopt = true, -- Whether to set the 'statuscolumn' option, may be set to false for those who
+        -- want to use the click handlers in their own 'statuscolumn': _G.Sc[SFL]a().
+        -- Although I recommend just using the segments field below to build your
+        -- statuscolumn to benefit from the performance optimizations in this plugin.
+        -- builtin.lnumfunc number string options
+        thousands = false,   -- or line number thousands separator string ("." / ",")
+        relculright = false, -- whether to right-align the cursor line number with 'relativenumber' set
+        -- Builtin 'statuscolumn' options
+        ft_ignore = nil,     -- lua table with filetypes for which 'statuscolumn' will be unset
+        bt_ignore = nil,     -- lua table with 'buftype' values for which 'statuscolumn' will be unset
+        -- Default segments (fold -> sign -> line number + separator), explained below
+        segments = {
+          -- { text = { builtin.foldfunc }, click = "v:lua.ScSa" },
+          {
+            sign = {
+              name = { ".*" },
+              max_width = 2,
+              colwidth = 2,
+              auto = true,
+            },
+            -- condition = { true, builtin.not_empty },
+            click = "v:lua.ScLa",
+          },
+          {
+            text = { builtin.lnumfunc, },
+            -- condition = { true, builtin.not_empty },
+            click = "v:lua.ScLa",
+          },
+          {
+            -- Ref: https://github.com/luukvbaal/statuscol.nvim/issues/71
+            sign = {
+              name = { "GitSign*" },
+              max_width = 2,
+              colwidth = 1,
+              auto = true,
+            },
+            -- condition = { true, builtin.not_empty },
+            click = "v:lua.ScLa",
+          },
+        },
+        clickmod = "c",   -- modifier used for certain actions in the builtin clickhandlers:
+        -- "a" for Alt, "c" for Ctrl and "m" for Meta.
+        clickhandlers = { -- builtin click handlers
+          Lnum                    = builtin.lnum_click,
+          FoldClose               = builtin.foldclose_click,
+          FoldOpen                = builtin.foldopen_click,
+          FoldOther               = builtin.foldother_click,
+          DapBreakpointRejected   = builtin.toggle_breakpoint,
+          DapBreakpoint           = builtin.toggle_breakpoint,
+          DapBreakpointCondition  = builtin.toggle_breakpoint,
+          DiagnosticSignError     = builtin.diagnostic_click,
+          DiagnosticSignHint      = builtin.diagnostic_click,
+          DiagnosticSignInfo      = builtin.diagnostic_click,
+          DiagnosticSignWarn      = builtin.diagnostic_click,
+          GitSignsTopdelete       = builtin.gitsigns_click,
+          GitSignsUntracked       = builtin.gitsigns_click,
+          GitSignsAdd             = builtin.gitsigns_click,
+          GitSignsChange          = builtin.gitsigns_click,
+          GitSignsChangedelete    = builtin.gitsigns_click,
+          GitSignsDelete          = builtin.gitsigns_click,
+          gitsigns_extmark_signs_ = builtin.gitsigns_click,
+        },
+      })
+    end,
+  },
   -- { -- gxt_kt vim-tmux-clipboard : vim tmux clipboard
   --   'roxma/vim-tmux-clipboard',
   -- },
