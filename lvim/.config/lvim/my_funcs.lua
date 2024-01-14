96f0cf1fed1ad0e1ac8e4f0921c1e32841ed2a64
gxt_kt <gxt_kt@163.com>
2024-01-14 22:04:28 +0800

    lvim add new function that save file use sudo

--- lvim/.config/lvim/my_funcs.lua
+++ lvim/.config/lvim/my_funcs.lua
@@ -212,4 +212,45 @@ M.GetBufName = function()
 end
 
 
+-- Use like ":w !sudo tee %" to save file without root
+-- Ref: https://github.com/neovim/neovim/issues/12103
+M.SudoWrite = function(tmpfile, filepath)
+  if not tmpfile then tmpfile = vim.fn.tempname() end
+  if not filepath then filepath = vim.fn.expand("%") end
+  if not filepath or #filepath == 0 then
+    M.err("E32: No file name")
+    return
+  end
+  -- `bs=1048576` is equivalent to `bs=1M` for GNU dd or `bs=1m` for BSD dd
+  -- Both `bs=1M` and `bs=1m` are non-POSIX
+  local cmd = string.format("dd if=%s of=%s bs=1048576",
+    vim.fn.shellescape(tmpfile),
+    vim.fn.shellescape(filepath))
+  -- no need to check error as this fails the entire function
+  vim.api.nvim_exec(string.format("write! %s", tmpfile), true)
+  if M.SudoExec(cmd) then
+    -- vim.notify("\n\n\n", vim.log.levels.INFO, {})
+    vim.notify("\n" .. string.format([["%s" write successful!]], filepath), vim.log.levels.INFO, {})
+    vim.cmd("e!")
+  end
+  vim.fn.delete(tmpfile)
+end
+M.SudoExec = function(cmd, print_output)
+  vim.fn.inputsave()
+  local password = vim.fn.inputsecret("Password: ")
+  vim.fn.inputrestore()
+  if not password or #password == 0 then
+    M.warn("Invalid password, sudo aborted")
+    return false
+  end
+  local out = vim.fn.system(string.format("sudo -p '' -S %s", cmd), password)
+  if vim.v.shell_error ~= 0 then
+    print("\r\n")
+    M.err(out)
+    return false
+  end
+  if print_output then print("\r\n", out) end
+  return true
+end
+
 return M
