6e0ee6d8b0eee1585a109fd5ecf46ed6aa72b9cb
gxt_kt <gxt_kt@163.com>
2024-01-17 02:02:14 +0800

    sovle bug that show uv_is_closing

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -529,3 +529,12 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "python",
   command = "set tabstop=4  shiftwidth=4",
 })
+
+-- Sovle txt bug use kitty+tmux and show image
+-- https://github.com/neovim/neovim/issues/21856
+vim.api.nvim_create_autocmd({ "VimLeave" }, {
+  callback = function()
+    -- do nothing
+    vim.fn.jobstart('', {detach=true})
+  end,
+})
