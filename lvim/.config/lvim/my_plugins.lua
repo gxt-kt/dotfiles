83aad2ef7b177b2950eacc4650f24d38ac73f607
gxt_kt <gxt_kt@163.com>
2024-01-17 13:40:11 +0800

    update nvim hlsearch

--- lvim/.config/lvim/my_plugins.lua
+++ lvim/.config/lvim/my_plugins.lua
@@ -967,10 +967,21 @@ M.my_plugins = {
     'kevinhwang91/nvim-hlslens',
     config = function()
       require('hlslens').setup({
-        calm_down = true,
+        calm_down = false,
         nearest_only = true,
-        -- nearest_float_when = 'always'
+        nearest_float_when = 'never'
       })
+      local kopts = { noremap = true, silent = true }
+      vim.api.nvim_set_keymap('n', 'n',
+        [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
+        kopts)
+      vim.api.nvim_set_keymap('n', 'N',
+        [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
+        kopts)
+      vim.api.nvim_set_keymap('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+      vim.api.nvim_set_keymap('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+      vim.api.nvim_set_keymap('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+      vim.api.nvim_set_keymap('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
     end
   },
   {
