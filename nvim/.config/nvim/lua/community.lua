e36191a2de565b571dcbe6ed06bd206819db2793
gxt_kt <gxt_kt@163.com>
2024-05-11 04:04:22 +0800

    add noice plugin

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -9,4 +9,9 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+
+  -- noice默认自己安装会和astronvim不兼容
+  -- 所以我们用社区配置好的
+  -- https://www.reddit.com/r/AstroNvim/comments/16v81f0/help_needed_for_adding_noice_to_astronvim/
+  { import = "astrocommunity.utility.noice-nvim" },
 }
