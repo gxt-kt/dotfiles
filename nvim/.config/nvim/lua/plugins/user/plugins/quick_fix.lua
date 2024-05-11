a1489231df6b3c0541f8c88419ae3336ae99f15c
gxt_kt <gxt_kt@163.com>
2024-05-11 15:24:32 +0800

    add quick_fix

---
+++ nvim/.config/nvim/lua/plugins/user/plugins/quick_fix.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    -- 构建任务系统
+    -- https://github.com/skywind3000/asynctasks.vim/blob/master/README-cn.md
+    "skywind3000/asynctasks.vim",
+    config = function()
+    end
+  },
+  {
+    -- 构建任务系统
+    "skywind3000/asyncrun.vim",
+    config = function()
+    end
+  },
+  {
+    -- better quickfix
+    -- https://github.com/kevinhwang91/nvim-bqf
+    "kevinhwang91/nvim-bqf",
+    ft = 'qf',
+    config = function()
+      -- require("bqf").setup({
+      -- })
+    end,
+  },
+}
