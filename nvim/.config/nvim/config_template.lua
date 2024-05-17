02b93b2924405c616b36c0d71086a1f26ff686a1
gxt_kt <gxt_kt@163.com>
2024-05-17 15:47:34 +0800

    update

---
+++ nvim/.config/nvim/config_template.lua
@@ -0,0 +1,32 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- vim.opt.colorcolumn = "80" -- a line at 80 column postion -- 80 for google c++ style
+
+return {
+  notify = {
+    notify_enabled = false,
+  },
+  image = {
+    image_enabled = false,
+  },
+  gp = {
+    gp_enabled = true,
+    gp_proxy = "127.0.0.1:7890",
+    openai_api_key_string = "111", -- 有string优先str，没有就cat file_path文件
+    -- gp_openai_key_file_path = ""
+    -- gp_openai_key_file_path = vim.fn.stdpath "config" .. "/.openai_api_key",
+  },
+  markdown = {
+    highlight_by_headlines = true,
+  },
+  leetcode = {
+    leetcode_enabled = true,
+    show_image = true,
+    lang = "cpp",
+    cn = {
+      cn_enabled = true,
+      translator = false,
+      translate_problems = true,
+    },
+  },
+}
