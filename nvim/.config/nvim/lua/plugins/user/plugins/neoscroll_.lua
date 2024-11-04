7ff9431b0f6d35f28966aae7e9a140bd63c10492
gxt_kt <gxt_kt@163.com>
2024-11-04 20:44:33 +0800

    add neovim smooth animation

--- nvim/.config/nvim/lua/plugins/user/plugins/neoscroll_.lua
+++ nvim/.config/nvim/lua/plugins/user/plugins/neoscroll_.lua
@@ -19,7 +19,7 @@ return {
       stop_eof = true, -- Stop at <EOF> when scrolling downwards
       respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
       cursor_scrolls_alone = false, -- The cursor will keep on scrolling even if the window cannot scroll further
-      easing = "linear", -- Default easing function
+      easing = "linear", -- Default easing function ("linear", "quadratic", "cubic", "quartic", "quintic", "circular", "sine")
       pre_hook = nil, -- Function to run before the scrolling animation starts
       post_hook = nil, -- Function to run after the scrolling animation ends
       performance_mode = false, -- Disable "Performance Mode" on all buffers.
