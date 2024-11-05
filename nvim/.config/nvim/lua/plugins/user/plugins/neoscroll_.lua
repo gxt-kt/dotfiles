1361be89618ec76b172ae0f05deb0494ee0d4f33
gxt_kt <gxt_kt@163.com>
2024-11-05 10:54:10 +0800

    set cursor scroll

--- nvim/.config/nvim/lua/plugins/user/plugins/neoscroll_.lua
+++ nvim/.config/nvim/lua/plugins/user/plugins/neoscroll_.lua
@@ -18,7 +18,7 @@ return {
       hide_cursor = false, -- Hide cursor while scrolling
       stop_eof = true, -- Stop at <EOF> when scrolling downwards
       respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-      cursor_scrolls_alone = false, -- The cursor will keep on scrolling even if the window cannot scroll further
+      cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
       easing = "linear", -- Default easing function ("linear", "quadratic", "cubic", "quartic", "quintic", "circular", "sine")
       pre_hook = nil, -- Function to run before the scrolling animation starts
       post_hook = nil, -- Function to run after the scrolling animation ends
