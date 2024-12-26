72542a9938d9a4988e0c411b3f57e55903cf4ac8
gxt_kt <gxt_kt@163.com>
2024-12-26 20:03:06 +0800

    nvim add plugin: git-blame

--- nvim/.config/nvim/lua/plugins/user/plugins/git_.lua
+++ nvim/.config/nvim/lua/plugins/user/plugins/git_.lua
@@ -5,4 +5,21 @@ return {
     "FabijanZulj/blame.nvim",
     config = function() require("blame").setup() end,
   },
+  {
+    "f-person/git-blame.nvim",
+    -- load the plugin at startup
+    event = "VeryLazy",
+    -- Because of the keys part, you will be lazy loading this plugin.
+    -- The plugin wil only load once one of the keys is used.
+    -- If you want to load the plugin at startup, add something like event = "VeryLazy",
+    -- or lazy = false. One of both options will work.
+    opts = {
+      -- your configuration comes here
+      -- for example
+      enabled = true, -- if you want to enable the plugin
+      message_template = " [<date>] [<author>]:<summary>   <<sha>>", -- template for the blame message, check the Message template section for more options
+      date_format = "%Y-%m-%d %H:%M:%S", -- template for the date, check Date format section for more options
+      virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+    },
+  },
 }
