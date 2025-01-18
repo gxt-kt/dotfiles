3fbcc1afef4608793891085b85003cf39e5ec69f
gxt_kt <gxt_kt@163.com>
2025-01-18 19:42:03 +0800

    update nvim

--- nvim/.config/nvim/lua/plugins/user/plugins/flash_.lua
+++ nvim/.config/nvim/lua/plugins/user/plugins/flash_.lua
@@ -11,61 +11,59 @@ return {
             enabled = false,
           },
           char = {
-            enabled = true,
-            jump_labels = true,
+            enabled = false,
           },
         },
       }
     end,
   },
-  -- NOTE: gxt_kt now can remove hop plugin in next version
-  -- {
-  --   "phaazon/hop.nvim",
-  --   branch = "v2", -- optional but strongly recommended
-  --   event = "VeryLazy",
-  --   config = function()
-  --     -- you can configure Hop the way you like here; see :h hop-config
-  --     require("hop").setup { keys = "etovxqpdygfblzhckisuran" }
-  --   end,
-  --   init = function()
-  --     local hop = require "hop"
-  --     local directions = require("hop.hint").HintDirection
-  --     vim.keymap.set(
-  --       "",
-  --       "f",
-  --       function() hop.hint_char1 { direction = directions.AFTER_CURSOR, current_line_only = true } end,
-  --       { remap = true }
-  --     )
-  --     vim.keymap.set(
-  --       "",
-  --       "F",
-  --       function() hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = true } end,
-  --       { remap = true }
-  --     )
-  --     vim.keymap.set(
-  --       "",
-  --       "t",
-  --       function()
-  --         hop.hint_char1 {
-  --           direction = directions.AFTER_CURSOR,
-  --           current_line_only = false,
-  --           hint_offset = 0,
-  --         }
-  --       end,
-  --       { remap = true }
-  --     )
-  --     vim.keymap.set(
-  --       "",
-  --       "T",
-  --       function()
-  --         hop.hint_char1 {
-  --           direction = directions.BEFORE_CURSOR,
-  --           current_line_only = false,
-  --           hint_offset = 0,
-  --         }
-  --       end,
-  --       { remap = true }
-  --     )
-  --   end,
-  -- },
+  {
+    "phaazon/hop.nvim",
+    branch = "v2", -- optional but strongly recommended
+    event = "VeryLazy",
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require("hop").setup { keys = "etovxqpdygfblzhckisuran" }
+    end,
+    init = function()
+      local hop = require "hop"
+      local directions = require("hop.hint").HintDirection
+      vim.keymap.set(
+        "",
+        "f",
+        function() hop.hint_char1 { direction = directions.AFTER_CURSOR, current_line_only = true } end,
+        { remap = true }
+      )
+      vim.keymap.set(
+        "",
+        "F",
+        function() hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = true } end,
+        { remap = true }
+      )
+      vim.keymap.set(
+        "",
+        "t",
+        function()
+          hop.hint_char1 {
+            direction = directions.AFTER_CURSOR,
+            current_line_only = false,
+            hint_offset = 0,
+          }
+        end,
+        { remap = true }
+      )
+      vim.keymap.set(
+        "",
+        "T",
+        function()
+          hop.hint_char1 {
+            direction = directions.BEFORE_CURSOR,
+            current_line_only = false,
+            hint_offset = 0,
+          }
+        end,
+        { remap = true }
+      )
+    end,
+  },
 }
