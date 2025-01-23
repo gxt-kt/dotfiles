13908ea2cf364e028c8884a48a05908b399864ee
gxt_kt <gxt_kt@163.com>
2025-01-23 21:03:48 +0800

    add hexo snip for neovim

--- nvim/.config/nvim/lua/plugins/user/snippets.lua
+++ nvim/.config/nvim/lua/plugins/user/snippets.lua
@@ -1,16 +1,13 @@
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 return {
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      -- include the default astronvim config that calls the setup call
-      require "astronvim.plugins.configs.luasnip"(plugin, opts)
-      -- load snippets paths
-      require("luasnip.loaders.from_vscode").lazy_load {
-        vim.fn.stdpath "config" .. "/snippets",
-        -- require("my_sys").DEBUG(vim.fn.stdpath("config") .. "/snippets")
-      }
-    end,
-  },
+  "L3MON4D3/LuaSnip",
+  config = function(plugin, opts)
+    -- include the default astronvim config that calls the setup call
+    require "astronvim.plugins.configs.luasnip"(plugin, opts)
+    -- load snippets paths
+    require("luasnip.loaders.from_vscode").lazy_load {
+      paths = { vim.fn.stdpath "config" .. "/snippets" },
+    }
+  end,
 }
